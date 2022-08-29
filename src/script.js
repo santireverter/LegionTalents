709bf7581e76d02d9baa1460b47812c10b6f14f7
budhamud <nigitart@gmail.com>
2022-08-29 17:19:53 -0300

    servicios/footer y sass

--- src/script.js
+++ src/script.js
@@ -1,11 +1,37 @@
 let navbar = document.getElementById("navbar")
+let opB = document.getElementById("opB")
+let opC = document.getElementById("opC")
+let opD = document.getElementById("opD")
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {headerColor()};
 
-function myFunction() {
+function headerColor() {
   if (document.documentElement.scrollTop > 70) {
     navbar.classList.add("cambio")
   } else {
     navbar.classList.remove("cambio")
+  }
+}
+
+let count = 1;
+
+function carousel() {
+  if (count === 0) {
+    opB.classList.add("show")
+    opC.classList.remove("show")
+    opD.classList.remove("show")
+    count++
+  } else if (count === 1) {
+    opB.classList.remove("show")
+    opC.classList.add("show")
+    opD.classList.remove("show")
+    count++
+  } else if (count === 2) {
+    opB.classList.remove("show")
+    opC.classList.remove("show")
+    opD.classList.add("show")
+    count = 0
+  } else {
+    
   }
 }
