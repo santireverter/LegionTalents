e3e06e659f2fe70c8e196be1c20b38de8d889c4b
budhamud <nigitart@gmail.com>
2022-08-31 16:55:15 -0300

    carousel

--- src/script.js
+++ src/script.js
@@ -1,37 +1,42 @@
-let navbar = document.getElementById("navbar")
-let opB = document.getElementById("opB")
-let opC = document.getElementById("opC")
-let opD = document.getElementById("opD")
+let navbar = document.getElementById("navbar");
+let opB = document.getElementById("opB");
+let opC = document.getElementById("opC");
+let opD = document.getElementById("opD");
 
-window.onscroll = function() {headerColor()};
+window.onscroll = function () {
+  headerColor();
+};
 
 function headerColor() {
   if (document.documentElement.scrollTop > 70) {
-    navbar.classList.add("cambio")
+    navbar.classList.add("cambio");
   } else {
-    navbar.classList.remove("cambio")
+    navbar.classList.remove("cambio");
   }
 }
 
 let count = 1;
 
 function carousel() {
-  if (count === 0) {
-    opB.classList.add("show")
-    opC.classList.remove("show")
-    opD.classList.remove("show")
-    count++
-  } else if (count === 1) {
-    opB.classList.remove("show")
-    opC.classList.add("show")
-    opD.classList.remove("show")
-    count++
-  } else if (count === 2) {
-    opB.classList.remove("show")
-    opC.classList.remove("show")
-    opD.classList.add("show")
-    count = 0
-  } else {
-    
-  }
+    setTimeout(() => {
+      opB.classList.add("show");
+      opC.classList.remove("show");
+      opD.classList.remove("show");
+      count++;
+    }, 2000);
+    setTimeout(() => {
+      opB.classList.remove("show");
+      opC.classList.add("show");
+      opD.classList.remove("show");
+      count++;
+    }, 4000);
+    setTimeout(() => {
+    opB.classList.remove("show");
+    opC.classList.remove("show");
+    opD.classList.add("show");
+    count = 0;
+    carousel()
+  }, 6000);
 }
+
+carousel()
